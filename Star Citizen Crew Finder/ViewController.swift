8fde61266b12161dec9b7a4218c952457388a674
Kyler Gronner <kyler@landfx.com>
2020-03-24 19:07:12 -0700

    Initial Commit

---
+++ Star Citizen Crew Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Citizen Crew Finder
+//
+//  Created by Kyler Gronner on 3/24/20.
+//  Copyright © 2020 Kyler Gronner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
